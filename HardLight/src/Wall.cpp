414d5f5dce6896c2ac305b31d57fe06190313be7
muzuka <seanbrown7935@gmail.com>
2015-03-03 16:03:20 -0700

    Created new wall class

---
+++ HardLight/src/Wall.cpp
@@ -0,0 +1,35 @@
+
+#include "Wall.h"
+#include <glm/glm.hpp>
+#include <glm\gtx\rotate_vector.hpp>
+
+Wall::Wall() {
+}
+	
+Wall::Wall(PxRigidActor* init_actor, MeshData* init_mesh_data, const char* texture_file_path) {
+	type = UNDECLARED;
+	draw_mode = GL_TRIANGLES;
+	actor = init_actor;
+	mesh_data = init_mesh_data;
+	texture = load_tga_texture(texture_file_path);
+	init_opengl();
+}
+	
+Wall::~Wall() {
+}
+
+mat4 Wall::get_model_matrix() {
+	mat4 model_matrix = mat4(1.0);
+	PxTransform gPose = actor->getGlobalPose();
+	model_matrix = translate(model_matrix, vec3(gPose.p.x, gPose.p.y + 10.0f, gPose.p.z));
+	PxReal rads;
+	PxVec3 axis;
+	gPose.q.toRadiansAndUnitAxis(rads, axis);
+	float scaleFactor = 3.0f;
+
+	model_matrix = scale(model_matrix, vec3(scaleFactor, 0.1f, scaleFactor));
+
+	model_matrix = rotate(model_matrix, rads, vec3(axis.x, axis.y, axis.z));
+
+	return model_matrix;
+}
