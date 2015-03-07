61df931ab49cf9fda80e118a147678e2ccc4276e
muzuka <seanbrown7935@gmail.com>
2015-03-06 18:29:40 -0700

    implemented model initialization function; made Element a child of entity

--- HardLight/include/Element.h
+++ HardLight/include/Element.h
@@ -1,27 +1,27 @@
 #ifndef _ELEMENT_H_
 #define _ELEMENT_H_
 
+
 #include <SDL.h>
-#include <SDL_opengl.h>
 #include <SDL_Image.h>
-#include <GL/GL.h>
-#include <gl/GLU.h>
 #include <stdio.h>
 #include <string>
 #include <glm/glm.hpp>
+#include <glm\gtx\rotate_vector.hpp>
 
+#include "ShaderTools.h"
+#include "Entity.h"
 
 enum Action {
 	START,
 	QUIT
 };
 
-class Element {
+class Element : public Entity {
 protected:
-	glm::vec2 pos;
-	glm::vec2 size;
-	std::string  texturePath;
-	GLuint texture_id;
+	vec2 pos;
+	vec2 size;
+	string  texturePath;
 	SDL_Surface* texture;
 
 public:
@@ -29,15 +29,17 @@ public:
 	Element(const char*);
 	~Element();
 
-	glm::vec2 getPos();
-	glm::vec2 getSize();
+	vec2 getPos();
+	vec2 getSize();
 	SDL_Surface* getSurface();
-	std::string getPath();
+	string getPath();
 	GLuint getTextureID();
 	void init_texture();
-	void setPos(glm::vec2);
-	void setSize(glm::vec2);
+	void init_model(SDL_Window*);
+	void setPos(vec2);
+	void setSize(vec2);
 	void setSize(double, double);
+	virtual mat4 get_model_matrix();
 };
 
 #endif
