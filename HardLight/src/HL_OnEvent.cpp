e374f10fcdf88f52e413454e6040a27563a17bf0
muzuka <seanbrown7935@gmail.com>
2015-03-06 18:33:56 -0700

    updated gui function calls

--- HardLight/src/HL_OnEvent.cpp
+++ HardLight/src/HL_OnEvent.cpp
@@ -110,7 +110,7 @@ void HardLight::OnEvent(SDL_Event* Event)
 				break;
 			case SDL_CONTROLLER_BUTTON_START: // START button
 				scene = PAUSE;
-				gui.loadMenu("../Paused.txt");
+				gui.loadMenu("../Paused.txt", pxAgent->get_physics());
 				sfxMix.PlaySoundEffect("sfxItemUsed");
 				break;
 			case SDL_CONTROLLER_BUTTON_BACK:
