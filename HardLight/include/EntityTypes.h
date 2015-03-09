148597f529becfe66cfa440a2926855897fea1cd
achu <achu@coverity.com>
2015-03-09 17:19:49 -0600

    Fixed regression where tails were no longer detected as valid collisions

--- HardLight/include/EntityTypes.h
+++ HardLight/include/EntityTypes.h
@@ -3,15 +3,15 @@
 
 enum EntityTypes
 {
-	GROUND,
-	WHEEL,
-	BIKE,
-	WALL,
-	UNDECLARED,
-	SKYBOX,
-	PICKUP,
-	FLOOR,
-	MENU_PANEL,
+	GROUND		= 1 << 0,
+	WHEEL		= 1 << 1,
+	BIKE		= 1 << 2,
+	WALL		= 1 << 3,
+	UNDECLARED	= 1 << 4,
+	SKYBOX		= 1 << 5,
+	PICKUP		= 1 << 6,
+	FLOOR		= 1 << 7,
+	MENU_PANEL		= 1 << 8
 };
 
 enum BikeSubTypes
