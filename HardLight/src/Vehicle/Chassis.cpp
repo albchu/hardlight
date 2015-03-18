f9ac0857fc8a72d45aa6382c043c9b7875633d37
achu <achu@coverity.com>
2015-03-18 14:43:10 -0600

    New driving model and config file has been updated

--- HardLight/src/Vehicle/Chassis.cpp
+++ HardLight/src/Vehicle/Chassis.cpp
@@ -29,7 +29,7 @@ mat4 Chassis::get_model_matrix()
 	//model_matrix = rotate(model_matrix, rads * (axis.y), vec3(0, 1, 0));
 	//model_matrix = rotate(model_matrix, (rads * normalize(axis.z)) * -5.0f, vec3(0, 0, 1));
 
-	//model_matrix = scale(model_matrix, vec3(2.5,2.5,2.5));
+	model_matrix = scale(model_matrix, vec3(2.5,2.5,2.5));
 
 	return model_matrix;
 }
