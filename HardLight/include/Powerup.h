30e144fabbe37c7ccb8e63b90d15e953fba7d8ad
achu <achu@coverity.com>
2015-03-24 01:06:04 -0600

    Cleaned up powerup class

--- HardLight/include/Powerup.h
+++ HardLight/include/Powerup.h
@@ -2,48 +2,12 @@
 #define _POWERUP_H
 #include "Vehicle\Chassis.h"
 #include "Vehicle\TailWall.h"
-//#include "Vehicle\BikeManager.h"
-//#include "Vehicle\TailWall.h"
 
 #include "../inih\cpp\INIReader.h"
-
-enum PowerupType
-{
-	INSTANT,
-	HOLD
-};
 
 template <class T> class Powerup
 {
 public:
 	typedef void (T::*PowerCallback)(Chassis* chassis, TailWall* tailwall, INIReader* config);
-	Powerup();
-	//Powerup(Bike* bike, BikeManager* bike_manager, INIReader* config);
-	~Powerup();
-
-	//Bike* getBike();
-	//BikeManager* getBikes();
-	//INIReader* getINIReader();
-
-	//void setBike(Bike* bike);
-	//void setBikes(BikeManager* bike_manager);
-	//void setINIReader(INIReader* config);
-
-	//void choosePowerup(); // determine the type of powerup the Powerup object will be
-	//int usePowerup(); // tell bike to use a powerup (generic version)
-	//void jump(Chassis* chassis, TailWall* tailwall, INIReader* config);
-private:
-
-protected:
-	PowerupType type;
-	//Bike* bike;
-	//BikeManager* bike_manager;
-
-	//INIReader* config;
-
-
-	//// Specific usePowerup functions
-	//void useExtendTail();
-	//void useJump();
 };
 #endif
