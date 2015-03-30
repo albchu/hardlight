470c9a54bc5da86dee5db5256dcad75209622210
Kevin Tolsma <kevin.tolsma@gmail.com>
2015-03-29 20:40:02 -0600

    Revert "Added particle creation to onContact"
    This reverts commit dd9dd5666b829f411585b114d579f341343e86c7.

--- HardLight/src/HL_OnContact.cpp
+++ HardLight/src/HL_OnContact.cpp
@@ -1,4 +1,4 @@
-﻿//==============================================================================
+//==============================================================================
 #include "HardLight.h"
 
 //==============================================================================
@@ -14,8 +14,10 @@ void HardLight::onContact(const PxContactPairHeader& pairHeader, const PxContact
 			Bike* bike2 = bike_manager->get_bike(pairHeader.actors[1]);
 			if (bike1 != NULL)
 			{
-				if(find(bikesToKill.begin(), bikesToKill.end(), bike1) == bikesToKill.end())	// Only push bike onto bikes to kill vector if it does not already exist
+				if(find(bikesToKill.begin(), bikesToKill.end(), bike1) == bikesToKill.end())
+				{	// Only push bike onto bikes to kill vector if it does not already exist
 					bikesToKill.push_back(bike1);
+				}
 				if(bike1->get_subtype() == PLAYER_BIKE)
 				{
 					((Player_Controller*)bike1->get_controller())->rumble(1.0, 120);
@@ -24,36 +26,14 @@ void HardLight::onContact(const PxContactPairHeader& pairHeader, const PxContact
 			if (bike2 != NULL)
 			{
 				if(find(bikesToKill.begin(), bikesToKill.end(), bike2) == bikesToKill.end())
+				{
 					bikesToKill.push_back(bike2);
+				}
 				if(bike2->get_subtype() == PLAYER_BIKE)
 				{
 					((Player_Controller*)bike2->get_controller())->rumble(1.0, 120);
 				}
 			}
-
-			PxTransform collisionTransform;
-
-			collisionTransform = bike1->get_chassis()->get_actor()->getGlobalPose();
-
-			PxVec3 collisionPosition = PxVec3(collisionTransform.p.x, collisionTransform.p.y, collisionTransform.p.z);
-
-			// initialize creation data: random velocities and directions
-			particleCreationData = ParticleFactory::createRandomParticleData(maxParticles, particleSpeed, &particleData, collisionPosition);
-
-			// initialize particle system
-			particleSystem = ParticleFactory::createParticles(maxParticles, pxAgent->get_physics(), particleCreationData);
-
-			// add to scene
-			if(particleSystem)
-				pxAgent->get_scene()->addActor(*particleSystem);
-
-			particleSystem->addForces(maxParticles,PxStrideIterator<const PxU32> (particleData.getIndexes()),PxStrideIterator<PxVec3>(particleData.getForces()),PxForceMode::eFORCE);
-
-			ParticleSystem* particleEntity = new ParticleSystem(pxAgent->get_physics()->createRigidStatic(PxTransform(PxVec3(0.0f, 10.0f, 0.0f))), ParticleFactory::createMeshData(particleSystem), TextureMap::Instance()->getTexture("../data/Textures/PowerUpRed.tga"));
-
-			particleEntity->setParticleSystem(particleSystem);
-
-			world.add_entity(particleEntity);
 		}
 	}
 }
